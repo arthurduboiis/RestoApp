c5c86eba125f546bfe6c47524aa0dd3c729ea107
Arthur dubois <arthur.dbs70@gmail.com>
2022-11-16 08:07:22 +0100

    Initial Commit

---
+++ RestoApp/RestoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestoAppApp.swift
+//  RestoApp
+//
+//  Created by Arthur dubois on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
